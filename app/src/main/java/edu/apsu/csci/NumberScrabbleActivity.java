e58894844d074c14c15ce378ecee60ed12c67c05
Keenya22 <keenyagilchrist@hotmail.com>
2019-01-28 21:51:15 -0600

    Added pages Number Scrabble and Random Turn.

---
+++ app/src/main/java/edu/apsu/csci/NumberScrabbleActivity.java
@@ -0,0 +1,12 @@
+package edu.apsu.csci;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NumberScrabbleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_scrabble);
+    }
+}
