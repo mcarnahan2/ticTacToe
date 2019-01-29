a261ad29fadf537effb03c40b92752125f68acc7
Keenya22 <keenyagilchrist@hotmail.com>
2019-01-28 21:46:18 -0600

    Added pages Number Scrabble and Random Turn.

--- app/src/main/java/edu/apsu/csci/MainActivity.java
+++ app/src/main/java/edu/apsu/csci/MainActivity.java
@@ -1,7 +1,11 @@
 package edu.apsu.csci;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.number_scrabble_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), NumberScrabbleActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        b = findViewById(R.id.random_turn_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RandomTurnActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        b = findViewById(R.id.about_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToWebsite("https://en.wikipedia.org/wiki/Tic-tac-toe_variants");
+            }
+        });
+
+
+
     }
+
+    private void goToWebsite(String url) {
+        Uri uri = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
 }
