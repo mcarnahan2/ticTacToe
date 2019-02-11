ab4661d334477e1f17e146a2a7074df3854a953e
mcarn <mlcarnahan22@gmail.com>
2019-02-11 14:04:32 -0600

    Added an if statement to determine the players turn.

--- app/src/main/java/edu/apsu/csci/NumberScrabbleActivity.java
+++ app/src/main/java/edu/apsu/csci/NumberScrabbleActivity.java
@@ -11,17 +11,12 @@ import java.util.HashMap;
 
 public class NumberScrabbleActivity extends AppCompatActivity {
     private HashMap<Integer, Integer> tttMap;
+    private int count=2;
     private int [] ids = {
         R.id.editText00, R.id.editText01, R.id.editText02,
         R.id.editText10, R.id.editText11, R.id.editText12,
         R.id.editText20, R.id.editText21, R.id.editText22
     };
-    private int [] tvIds = {
-            R.id.textView00, R.id.textView01, R.id.textView02,
-            R.id.textView10, R.id.textView11, R.id.textView12,
-            R.id.textView20, R.id.textView21, R.id.textView22
-    };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,43 +34,40 @@ public class NumberScrabbleActivity extends AppCompatActivity {
         tttMap.put(R.id.editText21, R.id.textView21);
         tttMap.put(R.id.editText22, R.id.textView22);
 
-        findViewById(R.id.submit_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText editText;
-                for(int id : ids){
-                    if(tttMap.containsKey(id)) {
-                        editText = findViewById(id);
-                        String etStr = editText.getText().toString();
+        findViewById(R.id.submit_button).setOnClickListener(new SubmitListener());
+    }
 
-                        Log.i("IDS", "Edit text string is " + etStr);
+    class SubmitListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            TextView textView = findViewById(R.id.textView);
+            if(count % 2 == 0){
+                textView.setText("Player 2's Turn");
+            } else {
+                textView.setText("Player 1's Turn");
+            }
+            count+=1;
+            EditText editText;
+            for(int id : ids){
+                if(tttMap.containsKey(id)) {
+                    editText = findViewById(id);
+                    String etStr = editText.getText().toString();
 
-                        if(!etStr.equals("")) {
-                            int tvId = tttMap.get(id);
-                            TextView tv = findViewById(tvId);
-                            tv.setText(etStr);
-                            tv.setVisibility(View.VISIBLE);
-                            editText.setVisibility(View.INVISIBLE);
-                            String tvStr = tv.getText().toString();
-                            Log.i("IDS", "Text view string is " + tvStr);
-                        }
+                    Log.i("IDS", "Edit text string is " + etStr);
 
-                    }
-
-                    /*
-                        Works but trying to use hashmap
-                        if(!etStr.equals("")){
-                        for(int tid : tvIds){
-                            tv = findViewById(tid);
-                            tv.setText(etStr);
-                            String tvStr = tv.getText().toString();
-                            Log.i("IDS", "Text view string is " + tvStr);
-                        }
-                    */
-
+                    if(!etStr.equals("")) {
+                        int tvId = tttMap.get(id);
+                        TextView tv = findViewById(tvId);
+                        tv.setText(etStr);
+                        tv.setVisibility(View.VISIBLE);
+                        editText.setVisibility(View.INVISIBLE);
+                        String tvStr = tv.getText().toString();
+                        Log.i("IDS", "Text view string is " + tvStr);
                     }
 
                 }
-        });
+
+            }
+        }
     }
 }
