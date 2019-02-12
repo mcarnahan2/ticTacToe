aaf1dd9efd895e26b29f191fdbbe69dffc9ccfd9
mcarn <mlcarnahan22@gmail.com>
2019-02-11 22:34:34 -0600

    Used if statements to determine if the number is between 1 and 9 and mak sure numbers are not repeated.

--- app/src/main/java/edu/apsu/csci/NumberScrabbleActivity.java
+++ app/src/main/java/edu/apsu/csci/NumberScrabbleActivity.java
@@ -14,7 +14,8 @@ import java.util.HashMap;
 public class NumberScrabbleActivity extends AppCompatActivity {
     private HashMap<Integer, Integer> tttMap;
     private ArrayList<Integer> numbersUsed;
-    private int count=2;
+    private int count=0;
+    private int it = 0;
     private int [] ids = {
         R.id.editText00, R.id.editText01, R.id.editText02,
         R.id.editText10, R.id.editText11, R.id.editText12,
@@ -47,20 +48,64 @@ public class NumberScrabbleActivity extends AppCompatActivity {
         public void onClick(View view) {
             TextView textView = findViewById(R.id.textView);
             if(count % 2 == 0){
-                textView.setText("Player 2's Turn");
-            } else {
                 textView.setText("Player 1's Turn");
+            } else {
+                textView.setText("Player 2's Turn");
             }
             count+=1;
             EditText editText;
+            String etStr;
+
             for(int id : ids){
                 if(tttMap.containsKey(id)) {
                     editText = findViewById(id);
-                    String etStr = editText.getText().toString();
+                    Log.i("WHERE", "In if statement contains key id is " + id);
 
-                    Log.i("IDS", "Edit text string is " + etStr);
+                    etStr = editText.getText().toString();
 
-                    if(!etStr.equals("")) {
+
+                    if(etStr.trim().length()>0){
+                        int etNum = 0;
+                        try{
+                            etNum = Integer.parseInt(etStr);
+                        } catch(NumberFormatException ex){
+                            Log.i("IDS", "Invalid integer format");
+                        }
+
+                        int tvId = tttMap.get(id);
+                        TextView tv = findViewById(tvId);
+
+                        if(etNum>=1 && etNum<=9){
+                            if(numbersUsed.isEmpty()){
+                                tv.setText(etStr);
+                                tv.setVisibility(View.VISIBLE);
+                                editText.setVisibility(View.INVISIBLE);
+                                numbersUsed.add(etNum);
+                                Log.i("WHERE", "In empty " + etNum);
+                            } else {
+                                if(numbersUsed.contains(etNum)  && editText.isShown()) {
+                                    Toast.makeText(getApplicationContext(), "Please enter a number that has not been entered already", Toast.LENGTH_SHORT).show();
+                                    Log.i("WHERE", "In contains " + etNum);
+                                } else {
+                                    tv.setText(etStr);
+                                    tv.setVisibility(View.VISIBLE);
+                                    editText.setVisibility(View.INVISIBLE);
+                                    Log.i("WHERE", "In else " + etNum);
+                                }
+                            }
+                        } else{
+                            Toast.makeText(getApplicationContext(), "Not in the range", Toast.LENGTH_SHORT).show();
+                        }
+
+                        String tvStr = tv.getText().toString();
+                    }
+                }
+            }
+        }
+    }
+}
+
+/*if(!etStr.equals("")) {
                         int etNum = 0;
                         try{
                             etNum = Integer.parseInt(etStr);
@@ -73,23 +118,14 @@ public class NumberScrabbleActivity extends AppCompatActivity {
                         int tvId = tttMap.get(id);
                         TextView tv = findViewById(tvId);
                         if(etNum>=1 && etNum<=9){
-                            for(int num : numbersUsed){
-                                
-                            }
                             tv.setText(etStr);
                             tv.setVisibility(View.VISIBLE);
                             editText.setVisibility(View.INVISIBLE);
+
                         } else{
                             Toast.makeText(getApplicationContext(), "Not in the range", Toast.LENGTH_SHORT).show();
                         }
 
                         String tvStr = tv.getText().toString();
                         Log.i("IDS", "Text view string is " + tvStr);
-                    }
-
-                }
-
-            }
-        }
-    }
-}
+                    }*/
